50a6b86171a001cfff1d06dd7d31c66c50d949db
Cliffield <bauerfelix@web.de>
2017-11-06 18:28:31 +0100

    remove gosu

--- launch.sh
+++ launch.sh
@@ -2,4 +2,4 @@
 
 # launch the APP
 echo "[INFO] Launching $APP_NAME ..."
-gosu $PUSER:$PGROUP bash -c "/usr/bin/python $APP_HOME/app/Headphones.py --host 0.0.0.0 --datadir $APP_HOME/data --config $APP_HOME/config/headphones.ini"
+su-exec $PUSER:$PGROUP bash -c "/usr/bin/python $APP_HOME/app/Headphones.py --host 0.0.0.0 --datadir $APP_HOME/data --config $APP_HOME/config/headphones.ini"
